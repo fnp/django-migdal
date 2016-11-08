99c8259adb407042592ae3d1879a6617ca29add1
Jan Szejko <janek37@gmail.com>
2016-11-08 16:33:00 +0100

    version 0.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def whole_trees(package_dir, paths):
 
 setup(
     name='django-migdal',
-    version='0.4',
+    version='0.5',
     author='Radek Czajka',
     author_email='radoslaw.czajka@nowoczesnapolska.org.pl',
     url='',
