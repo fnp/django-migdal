6f42e02b4415e5f822739a47a7533cbb96794bcb
Jan Szejko <janek37@gmail.com>
2017-02-28 12:53:00 +0100

    fix haystack compatibility

--- migdal/search_indexes.py
+++ migdal/search_indexes.py
@@ -19,7 +19,7 @@ class EntryIndex(indexes.SearchIndex, indexes.Indexable):
     def get_model(self):
         return Entry
 
-    def index_queryset(self):
+    def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
 
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def whole_trees(package_dir, paths):
 
 setup(
     name='django-migdal',
-    version='0.5',
+    version='0.5.1',
     author='Radek Czajka',
     author_email='radoslaw.czajka@nowoczesnapolska.org.pl',
     url='',
