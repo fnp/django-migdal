c176b125612945be925d25922a6fe1db4d1def50
Radek Czajka <rczajka@rczajka.pl>
2019-10-02 22:05:58 +0200

    Fix bug with pagination usage.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def whole_trees(package_dir, paths):
 
 setup(
     name='django-migdal',
-    version='0.8.6',
+    version='0.8.7',
     author='Radek Czajka',
     author_email='radoslaw.czajka@nowoczesnapolska.org.pl',
     url='',
